2b8b6657f63b030fab83c8e50fe759a5c8bc81c1
Krish Thakur <krishtracy@gmail.com>
2025-05-30 01:57:31 -0700

    Add structured light pattern files

---
+++ create-structured-light-pattern/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fullscreen',
+    version='1.1.0',
+    description='Display the image in full screen',
+    url='https://github.com/elerac/fullscreen',
+    author='Ryota Maeda',
+    author_email='maeda.ryota.elerac@gmail.com',
+    license='MIT',
+    py_modules=['fullscreen'],
+    packages=find_packages()
+)
